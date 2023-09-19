d9c6cbdba30c8a1620a386fc88463d6e66cffd09
EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-09-19 01:20:39 -0500

    feat: :art: Submit para envio de datos

--- Biblioteca/src/components/login/Login.jsx
+++ Biblioteca/src/components/login/Login.jsx
@@ -4,22 +4,28 @@ import {EyeSlashFilledIcon} from "./EyeSlashFilledIcon";
 import {Input} from "@nextui-org/react";
 import {Button} from "@nextui-org/react";
 
+
 export default function Login() {
-  const [value, setValue] = React.useState("");
+  const [email, setemail] = React.useState("");
   const [isVisible, setIsVisible] = React.useState(false);
-  const validateEmail = (value) => value.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
+  const validateEmail = (email) => email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$/i);
   const toggleVisibility = () => setIsVisible(!isVisible);
 
   const isInvalid = React.useMemo(() => {
-    if (value === "") return false;
+    if (email === "") return false;
 
-    return validateEmail(value) ? false : true;
-  }, [value]);
+    return validateEmail(email) ? false : true;
+  }, [email]);
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const  query = Object.fromEntries( new window.FormData(event.target))
+    console.log(query);
+  };
   return (
     
 
-    <form className="flex flex-col justify-center items-center shadow-md rounded px-8 pt-6 pb-8">
+    <form className="flex flex-col justify-center items-center shadow-md rounded px-8 pt-6 pb-8" onSubmit={handleSubmit}>
 
         <h1 className="text-4xl font-extrabold text-center mb-6" style={{ fontFamily: 'Futura, sans-serif' }}>
           Login Usuarios
@@ -27,18 +33,20 @@ export default function Login() {
 
         <Input 
           key="email"
-          value={value}
+          name = "email"
+          value={email}
           type="email"
           label="Email"
           variant="bordered"
           isInvalid={isInvalid}
           color={isInvalid ? "danger" : "success"}
           errorMessage={isInvalid && "Please enter a valid email"}
-          onValueChange={setValue}
+          onValueChange={setemail}
           className="max-w-xs mb-4"
         />
         <Input 
           key="password"
+          name = "password"
           label="Password"
           variant="bordered"
           placeholder="Enter your password"
